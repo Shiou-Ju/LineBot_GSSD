8507810b8509e33721b111381c9b84ea0ef072f6
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2021-06-13 13:50:25 +0800

    初步建置
    - 搭配 ngrok
    - 可以hoot上

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LineBot_GSSD
 LineBot for GSSD Service
+
+## 使用方法
+- npm install or yarn add
+- 在envExample 中輸入LINE 提供的資訊，並更改名稱
+- node app.js 
+- 搭配使用ngrok 進行測試
---
+++ app.js
@@ -0,0 +1,31 @@
+// 引用linebot SDK
+const linebot = require("linebot");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+// 用於辨識Line Channel的資訊
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_TOEKN,
+});
+
+// 當有人傳送訊息給Bot時
+bot.on("message", function (event) {
+  // event.message.text是使用者傳給bot的訊息
+  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
+  event
+    .reply(`測試回覆訊息是：${event.message.text}`)
+    .then(function (data) {
+      // 當訊息成功回傳後的處理
+    })
+    .catch(function (error) {
+      // 當訊息回傳失敗後的處理
+    });
+});
+
+// Bot所監聽的webhook路徑與port
+bot.listen("/linewebhook", 3000, function () {
+  console.log("[BOT已準備就緒]");
+});
