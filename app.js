08b286ea2e1e33702a625be7f35ab406c87f8e85
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2021-06-13 14:45:11 +0800

    導入Nodemon
    - 增加訊息複雜度
    - npm run dev

--- app.js
+++ app.js
@@ -11,18 +11,46 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_TOEKN,
 });
 
+// 加入好友顯示的歡迎的訊息
+bot.on("follow", function (event) {
+  event.reply(
+    `感謝您使用公示送達LINE BOT服務。\n您可以輸入應受送達人之名稱，系統將會自動顯示該送達人最近三筆的公示送達資料。\n\n使用方式：\n請輸入「搜尋 名字」（中間為半形空格）\n例如：「搜尋 王小明」`
+  );
+});;
+
+
 // 當有人傳送訊息給Bot時
 bot.on("message", function (event) {
-  // event.message.text是使用者傳給bot的訊息
-  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
-  event
-    .reply(`測試回覆訊息是：${event.message.text}`)
-    .then(function (data) {
-      // 當訊息成功回傳後的處理
-    })
-    .catch(function (error) {
-      // 當訊息回傳失敗後的處理
-    });
+  if(event.message.text.includes("搜尋 ")){
+    if(event.message.text.includes("年度")){
+
+    } else {
+      let addressee = event.message.text.split("搜尋 ")[1];
+      let queryStr =
+        `https://gssd-server.herokuapp.com/api/v1/posts?addressee=${addressee}&limit=3`
+      
+      event
+        .reply(`搜尋 ${addressee}的結果如下`)
+        .then(function (data) {})
+        .catch(function (error) {});
+    }
+  }
+
+  else if (event.message.text==="測試 "){
+    event
+      .reply(`測試訊息：\n${event.message.text}`)
+      .then(function (data) {})
+      .catch(function (error) {});
+  } 
+  
+  else {
+      event
+        .reply(
+          "抱歉您的訊息目前不支援\n\n公示送達LINE服務使用方式：\n1. 搜尋應受送達人：\n請輸入「搜尋 名字」\n（中間為半形空格）\n例如：「搜尋 王小明」\n\n2. 查詢特定判決字號：\n請輸入「搜尋 判決字號」\n（中間為半形空格）\n例如：「搜尋 109年度家繼訴字第1號」"
+        )
+        .then(function (data) {})
+        .catch(function (error) {});
+  }
 });
 
 // Bot所監聽的webhook路徑與port
