6556a5a423188aa1818eaec9ace6d3975cd08f31
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2021-06-13 15:54:02 +0800

    Before Editing the API info
    -  因為我的querystring不支援regex，要想辦法
    - 判決的話可以要求

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 引用linebot SDK
 const linebot = require("linebot");
 const dotenv = require("dotenv");
+const axios = require("axios");
 
 dotenv.config({ path: "./config.env" });
 
@@ -14,7 +15,7 @@ const bot = linebot({
 // 加入好友顯示的歡迎的訊息
 bot.on("follow", function (event) {
   event.reply(
-    `感謝您使用公示送達LINE BOT服務。\n您可以輸入應受送達人之名稱，系統將會自動顯示該送達人最近三筆的公示送達資料。\n\n使用方式：\n請輸入「搜尋 名字」（中間為半形空格）\n例如：「搜尋 王小明」`
+    `感謝您使用公示送達LINE BOT服務。\n\n您可以輸入名字，系統將會自動顯示該名字對應之送達人最近三筆的公示送達資料；也可以輸入完整的字號來查詢該案件最近三筆的公示送達公告。\n\n公示送達LINE服務使用方式：\n1. 搜尋應受送達人：\n請輸入「搜尋 名字」\n（中間為半形空格）\n例如：「搜尋 王小明」\n\n2. 查詢特定判決字號：\n請輸入「搜尋 判決字號」\n（中間為半形空格）\n例如：「搜尋 109年度家繼訴字第1號」`
   );
 });;
 
@@ -22,13 +23,26 @@ bot.on("follow", function (event) {
 // 當有人傳送訊息給Bot時
 bot.on("message", function (event) {
   if(event.message.text.includes("搜尋 ")){
-    if(event.message.text.includes("年度")){
-
+    let queryStr = `https://gssd-server.herokuapp.com/api/v1/posts`;
+    if (event.message.text.includes("年度")) {
+      let caseName = event.message.text.split("搜尋 ")[1];
     } else {
       let addressee = event.message.text.split("搜尋 ")[1];
-      let queryStr =
-        `https://gssd-server.herokuapp.com/api/v1/posts?addressee=${addressee}&limit=3`
-      
+
+      axios
+        .get(queryStr, { params: { addressee: `${addressee}`, limit:  3 } })
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          /* 不論失敗成功皆會執行 */
+        });
+
+      // let resArray = [`搜尋 ${addressee}的結果如下`,1,2,3,4]
+
       event
         .reply(`搜尋 ${addressee}的結果如下`)
         .then(function (data) {})
@@ -46,7 +60,7 @@ bot.on("message", function (event) {
   else {
       event
         .reply(
-          "抱歉您的訊息目前不支援\n\n公示送達LINE服務使用方式：\n1. 搜尋應受送達人：\n請輸入「搜尋 名字」\n（中間為半形空格）\n例如：「搜尋 王小明」\n\n2. 查詢特定判決字號：\n請輸入「搜尋 判決字號」\n（中間為半形空格）\n例如：「搜尋 109年度家繼訴字第1號」"
+          "抱歉，您的訊息目前不支援！\n\n您可以輸入名字，系統將會自動顯示該名字對應之送達人最近三筆的公示送達資料；也可以輸入字號來查詢該案件之公示送達公告。\n\n公示送達LINE服務使用方式：\n1. 搜尋應受送達人：\n請輸入「搜尋 名字」\n（中間為半形空格）\n例如：「搜尋 王小明」\n\n2. 查詢特定判決字號：\n請輸入「搜尋 判決字號」\n（中間為半形空格）\n例如：「搜尋 109年度家繼訴字第1號」"
         )
         .then(function (data) {})
         .catch(function (error) {});
