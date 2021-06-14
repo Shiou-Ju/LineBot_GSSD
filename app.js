3bb431e907829a7c3f9f174e8af1faa86137bba6
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2021-06-14 12:20:00 +0800

    修改說明內容
    同時新增超過6個字元駁回

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const bot = linebot({
 // 加入好友顯示的歡迎的訊息
 bot.on("follow", function (event) {
   event.reply(
-    `感謝您使用公示送達LINE BOT服務！\n\n您可以輸入名字，系統將會顯示該名字對應之送達人最近三筆的公示送達資料；也可以輸入字號來查詢該案件最近三筆的公示送達公告。\n\n公示送達LINE服務使用方式：\n1. 搜尋應受送達人：\n請輸入「搜尋 名字」\n（中間為半形空格，名字不超過6個字）\n例如：「搜尋 王小明」\n例如：「搜尋 Steve」\n\n2. 查詢特定案件字號：\n請輸入「搜尋 案件字號」\n（中間為半形空格）\n例如：「搜尋 109年度訴字第1號」\n\n3. 查詢特定字號&特定日期之公告：\n請輸入「搜尋 案件&民國年-月-日」\n例如：「搜尋 109年度訴字第1號&110-01-01」`
+    `感謝您使用公示送達LINE BOT服務！\n\n您可以輸入名字，系統將會顯示該名字對應之最近三筆的公示送達資料；也可以輸入字號來查詢該案件最近三筆公示送達公告。\n\n公示送達LINE服務使用方式：\n1. 搜尋應受送達人：\n請輸入「搜尋 名字」\n（中間為半形空格，名字不超過6個字）\n例如：\n搜尋 王小明\n搜尋 Steve\n\n2. 查詢特定案件字號：\n請輸入「搜尋 案件字號」\n（中間為半形空格）\n例如：\n搜尋 109年度訴字第1號\n\n3. 查詢特定字號&特定日期之公告：\n請輸入「搜尋 案件字號&民國年-月-日」\n例如：\n搜尋 109年度訴字第1號&110-01-01`
   );
 });;
 
@@ -73,7 +73,9 @@ bot.on("message", function (event) {
             })
             .catch((error) => {
               console.error(error);
-              event.reply(`發生錯誤，尚在維修中，尚祈海涵！`);
+              event.reply(
+                `發生錯誤，尚在維修中，尚祈海涵！\n如有疑問，歡迎來信聯繫：\ngssd.subscription@gmail.com\n`
+              );
               console.log(`${event.source.userId} ${event.message.text}`);
             });
           // .finally(() => {
@@ -112,7 +114,9 @@ bot.on("message", function (event) {
             })
             .catch((error) => {
               console.error(error);
-              event.reply(`發生錯誤，尚在維修中，尚祈海涵！`);
+              event.reply(
+                `發生錯誤，尚在維修中，尚祈海涵！\n如有疑問，歡迎來信聯繫：\ngssd.subscription@gmail.com\n`
+              );
               console.log(`${event.source.userId} ${event.message.text}`);
             });
           //  .finally(() => {
@@ -124,41 +128,54 @@ bot.on("message", function (event) {
       else {
         let addressee = event.message.text.split("搜尋 ")[1].trim();
 
-        let threePosts = ``;
+        if (addressee.length > 6) {
+          event
+            .reply(`名字請勿輸入超過6個字，請再試一次`)
+            .then(function (data) {
+              console.log(`${event.source.userId} ${event.message.text}`);
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+        } else {
+          let threePosts = ``;
 
-        axios
-          .get(queryURL, { params: { addressee: `${addressee}` } })
-          .then((res) => {
-            let resLength = res.data.data.length;
-            if (resLength > 3) resLength = 3;
-            for (let i = 0; i < resLength; i++) {
-              threePosts += `〔第${i + 1}篇公告〕\n應受送達人姓名：${
-                res.data.data[i].addressee
-              }\n法院：${res.data.data[i].court}\n字號：${
-                res.data.data[i].title
-              }\n公告日期：${res.data.data[i].datePosted}\n公告網址：${
-                res.data.data[i].urlDetailed
-              }\n`;
-            }
-            event
-              .reply(
-                `搜尋 ${addressee} 最新${resLength}筆結果如下\n${threePosts}`
-              )
-              .then(function (data) {
-                console.log(`${event.source.userId} ${event.message.text}`);
-              })
-              .catch(function (error) {
-                console.error(error);
-              });
-          })
-          .catch((error) => {
-            console.error(error);
-            event.reply(`發生錯誤，尚在維修中，尚祈海涵！`);
-            console.log(`${event.source.userId} ${event.message.text}`);
-          });
-        // .finally(() => {
-        //   event.reply(`發生錯誤，尚在維修中，尚祈海涵！`)
-        // });
+          axios
+            .get(queryURL, { params: { addressee: `${addressee}` } })
+            .then((res) => {
+              let resLength = res.data.data.length;
+              if (resLength > 3) resLength = 3;
+              for (let i = 0; i < resLength; i++) {
+                threePosts += `〔第${i + 1}篇公告〕\n應受送達人姓名：${
+                  res.data.data[i].addressee
+                }\n法院：${res.data.data[i].court}\n字號：${
+                  res.data.data[i].title
+                }\n公告日期：${res.data.data[i].datePosted}\n公告網址：${
+                  res.data.data[i].urlDetailed
+                }\n`;
+              }
+              event
+                .reply(
+                  `搜尋 ${addressee} 最新${resLength}筆結果如下\n${threePosts}`
+                )
+                .then(function (data) {
+                  console.log(`${event.source.userId} ${event.message.text}`);
+                })
+                .catch(function (error) {
+                  console.error(error);
+                });
+            })
+            .catch((error) => {
+              console.error(error);
+              event.reply(
+                `發生錯誤，尚在維修中，尚祈海涵！\n如有疑問，歡迎來信聯繫：\ngssd.subscription@gmail.com\n`
+              );
+              console.log(`${event.source.userId} ${event.message.text}`);
+            });
+          // .finally(() => {
+          //   event.reply(`發生錯誤，尚在維修中，尚祈海涵！`)
+          // });
+        }
       }
     }
     // 測試字元
@@ -177,7 +194,7 @@ bot.on("message", function (event) {
     else {
       event
         .reply(
-          "抱歉，您的訊息目前不支援！\n\n您可以輸入名字，系統將會顯示該名字對應之送達人最近三筆的公示送達資料；也可以輸入字號來查詢該案件最近三筆的公示送達公告。\n\n公示送達LINE服務使用方式：\n1. 搜尋應受送達人：\n請輸入「搜尋 名字」\n（中間為半形空格，名字不超過6個字）\n例如：「搜尋 王小明」\n例如：「搜尋 Steve」\n\n2. 查詢特定案件字號：\n請輸入「搜尋 案件字號」\n（中間為半形空格）\n例如：「搜尋 109年度訴字第1號」\n\n3. 查詢特定字號&特定日期之公告：\n請輸入「搜尋 案件&民國年-月-日」\n例如：「搜尋 109年度訴字第1號&110-01-01」"
+          `抱歉，您的訊息目前不支援！\n\n您可以輸入名字，系統將會顯示該名字對應之最近三筆的公示送達資料；也可以輸入字號來查詢該案件最近三筆公示送達公告。\n\n公示送達LINE服務使用方式：\n1. 搜尋應受送達人：\n請輸入「搜尋 名字」\n（中間為半形空格，名字不超過6個字）\n例如：\n搜尋 王小明\n搜尋 Steve\n\n2. 查詢特定案件字號：\n請輸入「搜尋 案件字號」\n（中間為半形空格）\n例如：\n搜尋 109年度訴字第1號\n\n3. 查詢特定字號&特定日期之公告：\n請輸入「搜尋 案件字號&民國年-月-日」\n例如：\n搜尋 109年度訴字第1號&110-01-01`
         )
         .then(function (data) {
           console.log(`${event.source.userId} ${event.message.text}`);
